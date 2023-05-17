944e075943a7800c1300729d1bc649d88f9fbc69
amalsita <1608672@uab.cat>
2023-05-17 18:10:11 +0200

    HE QUITADO EL PRINT(HOLA HOLA HOLA)

--- data/deep_colorization.py
+++ data/deep_colorization.py
@@ -24,9 +24,9 @@ X = []
 for filename in os.listdir('imatges'):
 	X.append(img_to_array(load_img('imatges/'+filename)))
 X = np.array(X)
+
 print(X)
 
-print("HOLA HOLA HOLA HOLA")
 
 # Set up train and test data
 split = int(0.9*len(X))

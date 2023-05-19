a373781d63d178042d594ed8051c15915c2390b7
1605592 <1605592@uab.cat>
2023-05-19 14:57:54 +0000

    beta2.0

--- starting_point/Beta-version/beta_version.py
+++ starting_point/Beta-version/beta_version.py
@@ -4,18 +4,19 @@
 # In[ ]:
 
 
-from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
-from keras.layers import Activation, Dense, Dropout, Flatten, InputLayer
-from keras.layers.normalization import BatchNormalization
-from keras.callbacks import TensorBoard
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
+from keras.layers import Activation, Dense, Dropout, Flatten
+from tensorflow.keras.layers import BatchNormalization
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
 from skimage.io import imsave
 import numpy as np
 import os
 import random
 import tensorflow as tf
+
 
 
 # In[ ]:

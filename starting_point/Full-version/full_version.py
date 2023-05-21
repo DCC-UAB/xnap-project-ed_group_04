79974127047c1eeef9c245e90feb88b0aa4eef37
1605592 <1605592@uab.cat>
2023-05-21 10:06:09 +0000

    imports full version

--- starting_point/Full-version/full_version.py
+++ starting_point/Full-version/full_version.py
@@ -10,15 +10,15 @@ Original file is located at
 import keras
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.preprocessing import image
-from keras.engine import Layer
+#from keras.engine import Layer
 from keras.applications.inception_resnet_v2 import preprocess_input
-from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose, Input, Reshape, merge, concatenate
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose, Input, Reshape, concatenate
 from keras.layers import Activation, Dense, Dropout, Flatten
-from keras.layers.normalization import BatchNormalization
+#from keras.layers.normalization import BatchNormalization
 from keras.callbacks import TensorBoard 
 from keras.models import Sequential, Model
 from keras.layers.core import RepeatVector, Permute
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+#from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, gray2rgb
 from skimage.transform import resize
 from skimage.io import imsave
@@ -27,6 +27,18 @@ import os
 import random
 import tensorflow as tf
 
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
+from keras.layers import Activation, Dense, Dropout, Flatten
+from tensorflow.keras.layers import BatchNormalization
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
+from skimage.io import imsave
+import numpy as np
+import os
+import random
+import tensorflow as tf
 # Get images
 X = []
 for filename in os.listdir('Train/'):

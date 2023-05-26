70d61c67f667503dd8f74d2c09359313db56c662
amalsita <1608672@uab.cat>
2023-05-26 14:37:36 +0000

    dehrbsj

--- starting_point/Beta-version/3.Colorization_video.py
+++ starting_point/Beta-version/3.Colorization_video.py
@@ -1,229 +1,158 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 24 14:20:27 2020
-
-@author: ychen
-"""
-
-# =============================================================================
-#  0. Load Packages
-# =============================================================================
-
+#!/usr/bin/env python
+# coding: utf-8
+import matplotlib.pyplot as plt
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose, Input, Conv2D, UpSampling2D, MaxPooling2D
+from keras.layers import Activation, Dense, Dropout, Flatten
+from tensorflow.keras.layers import BatchNormalization
+from keras.models import Sequential
+from keras.callbacks import TensorBoard
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import array_to_img, img_to_array, load_img
 from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
 from skimage.io import imsave
 import numpy as np
-from skimage import io,color
-import matplotlib.pyplot as plt
-from os import walk
 import os
-from os.path import isfile, join
-import imutils
-import cv2
-from keras.models import model_from_json
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import random
 
-# =============================================================================
-# 1. Model
-# =============================================================================
-
-# =============================================================================
-# 1.1 Define Paths
-# =============================================================================
-### a. For Windows
-path=".images\\60-80"
-model_path = path+"\\saved model\\"
-
-
-### b. For Mac OS
-# path="images/60-80"
-# model_path=path+"/saved model/"
+import tensorflow as tf
+from skimage import img_as_ubyte
+#from tensorflow.keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from skimage.color import rgb2lab, lab2rgb
+from skimage.io import imsave
+from tensorflow.keras.layers import BatchNormalization, Conv2D, InputLayer, UpSampling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import tensorflow as tf
+from skimage import img_as_ubyte
+from PIL import Image
 
 
-# =============================================================================
-# 1.2 Some Functions
-# =============================================================================
+# Get images
+X = []
+for filename in os.listdir('starting_point/Beta-version/Paisajes_train/'):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        img = Image.open('starting_point/Beta-version/Paisajes_train/' + filename)
+        img = img.resize((256, 256))  # Asegurar que todas las imÃ¡genes tengan las mismas dimensiones
+        X.append(img_to_array(img))
+X = np.array(X, dtype=float)
 
-### a. Image Functions
 
-#Convert RGB images to LAB using CIELAB, then return input X (L), target Y (A. B)
-def rgbtolab_batch(path):
-    X=[]
-    Y=[]
-    j=1
-    row=[]
-    column=[]
-    image = []
-    files = [f for f in os.listdir(path) if isfile(join(path, f)) and f.endswith(".jpg")==True]
-    #for sorting the file names properly
-    files.sort(key = lambda x: int(x[5:-4]))
-    for i in range(len(files)):
-        filename=path + files[i]
-        image = img_to_array(load_img(filename))
-        image = np.array(image, dtype=float)
-        column.append(len(image[0]))
-        row.append(len(image))
-        image = cv2.resize(image, (80,60))
-        x = rgb2lab(1.0/255*image)[:,:,0]
-        y = rgb2lab(1.0/255*image)[:,:,1:]
-        y /= 128
-        X.append(x)
-        Y.append(y)
-        j +=1
-    return X,Y,row,column
 
-#Convert LAB channels to RGB for a list of image
-def labtorgb_batch(l,a,b,row,column):
-    rgb=[]
-    for i in range(len(l)):
-        lab=np.dstack([l[i],a[i],b[i]])
-        img=color.lab2rgb(lab)
-        img = cv2.resize(img, (column[i],row[i]))
-        rgb.append(img)
-    return rgb
+# Set up train and test data
+split = int(0.95 * len(X))
+Xtrain = X[:split]
+Xtrain = 1.0 / 255 * Xtrain
 
-#Save colorized image
-def savePredict(img_list,save_path):
-    for i in range(len(img_list)):
-        plt.imsave(("{}frame{}.jpg").format(save_path,i),img_list[i])
 
-### b. Load Pre-trained Model and Image
+model = Sequential()
+model.add(InputLayer(input_shape=(256, 256, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same', strides=2))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+model.add(UpSampling2D((2, 2)))
+model.compile(optimizer='adagrad', loss='mse', metrics=['accuracy'])
 
-#Load Model and weights
-def load_model(model_name):
-    json_file = open('{}.json'.format(model_name), 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    #load weights into new model
-    loaded_model.load_weights("{}.h5".format(model_name))
-    print("Loaded model from disk")
-    return loaded_model
 
-#Load Image
-def load_image(image_path):
-    X,Y,row,column = rgbtolab_batch(image_path)
-    n_test=len(X)
-    # Pre-process the data by reshaping
-    X=np.array(X)
-    Y=np.array(Y)
-    X = X.reshape(n_test, 60, 80, 1)
-    Y = Y.reshape(n_test, 60, 80, 2)
-    return X,Y,row,column
+#------------------------------------------DATA LOADER--------------------------------------------------------------
+# Image transformer   data augmentation
+datagen = ImageDataGenerator(
+        
+       
+        shear_range=0.2,
+        zoom_range=0.2,
+        rotation_range=20,
+        horizontal_flip=True, 
+        vertical_flip=True)
 
-#Load Model and Image - Colorization
-def load_all(model_name,image_path):
-    loaded_model=load_model(model_name)
-    X,Y,row,column=load_image(image_path)
-    n_test=len(X)
-    loaded_model.compile(optimizer='rmsprop',loss='mse',metrics=['accuracy'])
-    loaded_model.evaluate(X,Y,batch_size=10)
-    Y_predict=loaded_model.predict(X)
-    Y_predict *=128
-    Y_a=Y_predict[:,:,:,0].reshape(n_test,60,80,1)
-    Y_b=Y_predict[:,:,:,1].reshape(n_test,60,80,1)
-    Y_ori_a=Y[:,:,:,0].reshape(n_test,60,80,1)*128
-    Y_ori_b=Y[:,:,:,1].reshape(n_test,60,80,1)*128
-    rgb_list=labtorgb_batch(X,Y_a,Y_b,row,column)
-    rgb_original=labtorgb_batch(X,Y_ori_a,Y_ori_b,row,column)
-    return rgb_list,rgb_original
+# Generate training data
+batch_size = 10
+def image_a_b_gen(batch_size):
+    for batch in datagen.flow(Xtrain, batch_size=batch_size):  #entrena per bloc
+        lab_batch = rgb2lab(batch)
+        X_batch = lab_batch[:, :, :, 0]
+        Y_batch = lab_batch[:, :, :, 1:] / 128
+        yield (X_batch.reshape(X_batch.shape + (1,)), Y_batch)  #retorna cada bloc de dades de train
 
-# =============================================================================
-# 2. Colorizing Video
-# =============================================================================
 
-# =============================================================================
-# 2.1. Video Paths
-# =============================================================================
-VIDEO="Roman Holiday Short thumnail.mp4"
-bw_path = "video\\b&w_video_frames\\"
-save_path = "video\\colorized_video_frames\\"
-video_path = "video\\colorized_video\\"
+#-------------------------------------------------------------------------------------------------------------------------
+# Train model      
+tensorboard = TensorBoard(log_dir="output/first_run")
+history = model.fit_generator(image_a_b_gen(batch_size), callbacks=[tensorboard], epochs=5, steps_per_epoch=10)
 
-# =============================================================================
-# 2.2 Read video and convert to frames
-# =============================================================================
-video = "video\\"+VIDEO
-width = 80
-vs = cv2.VideoCapture(video)
+# Save model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
 
-# loop over frames from the video stream
-count = 0
-success = True
-while success:
-	# grab the next frame and handle if we are reading from either
-	# VideoCapture or VideoStream
-	success, frame = vs.read()
+# Process history
+losses = history.history['loss']
+accuracies = history.history['accuracy']
 
-	# if we are viewing a video and we did not grab a frame then we
-	# have reached the end of the video
-	if frame is None:
-		break
+# Plot learning curves
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(range(1, len(losses) + 1), losses)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Training Loss')
+plt.grid(True)
 
-	# resize the input frame, scale the pixel intensities to the
-	frame = imutils.resize(frame, 80)
-	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+plt.subplot(1, 2, 2)
+plt.plot(range(1, len(accuracies) + 1), accuracies)
+plt.xlabel('Iteration')
+plt.ylabel('Accuracy')
+plt.title('Training Accuracy')
+plt.grid(True)
 
-	# resize the frame to 80x60 (the dimensions of the  model
-	resized = cv2.resize(frame, (80, 60))
+plt.tight_layout()
+plt.savefig('starting_point/Beta-version/result/learning_curves.png')
+plt.close()
 
-	# show the original frames
-	cv2.imshow("Original", resized)
+gif_path = "starting_point/Beta-version/video-paisatge.gif"
+gif_image = Image.open(gif_path)
 
-    # save the black and white frames
-	cv2.imwrite(bw_path+"frame{}.jpg".format(str(count)), resized)
-	count += 1
-	key = cv2.waitKey(1) & 0xFF
+"""
+color_me = []
+for filename in os.listdir('starting_point/Beta-version/Paisajes2/'):
+    if filename.endswith(".git"):
+        img = Image.open('starting_point/Beta-version/' + filename)
+        img = img.resize((256, 256))
+        color_me.append(img_to_array(img))
+color_me = np.array(color_me, dtype=float)
+color_me = rgb2lab(1.0 / 255 * color_me)[:, :, :, 0]
+color_me = color_me.reshape(color_me.shape + (1,))
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+# Test model
+output = model.predict(color_me)
+output = output * 128
 
-vs.release()
+# Output colorizations
+for i in range(len(output)):
+    cur = np.zeros((256, 256, 3))
+    cur[:, :, 0] = color_me[i][:, :, 0]
+    cur[:, :, 1:] = output[i]
+    cur = lab2rgb(cur)
+   
+    imsave("starting_point/Beta-version/result/img_" + str(i) + ".gif", cur)
 
-# close any open windows
-cv2.destroyAllWindows()
-
-# =============================================================================
-#  2.3 Load model and colorize the video
-# =============================================================================
-
-# Load model on generated frames, colorize the frames and save colorized frames
-colorized, original = load_all(model_path+"model_0215",bw_path)
-savePredict(colorized,save_path)
-
-# =============================================================================
-#  2.4 Combine frames to a video file
-# =============================================================================
-
-### a. Define function to create a video from frames
-def convert_frames_to_video(pathIn, pathOut, fps):
-    frame_array = []
-    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f)) and f.endswith(".jpg")==True] #If it's a valid path and exclude .DS_Store
-
-    #for sorting the file names properly
-    files.sort(key = lambda x: int(x[5:-4]))
- 
-    for i in range(len(files)):
-        filename=pathIn + files[i]
-        #reading each files
-        img = cv2.imread(filename)
-        height, width, layers = img.shape
-        size = (width,height)
-        print(filename)
-        #inserting the frames into an image array
-        frame_array.append(img)
-
-    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'MJPG'), fps, size)
-
-    for i in range(len(frame_array)):
-        # writing to a image array
-        out.write(frame_array[i])
-    out.release()
-
-### b. Generate the video file
-pathIn= save_path
-pathOut = video_path+'video.avi'
-fps = 30.0
-convert_frames_to_video(pathIn, pathOut, fps)
-print("Colorized video is saved at {}".format(pathOut))
+"""
--- starting_point/Beta-version/beta_version.py
+++ starting_point/Beta-version/beta_version.py
@@ -32,16 +32,22 @@ from PIL import Image
 
 
 # Get images
-# TRAIN--------------------------------------------------------------------------------------------
 X = []
-for filename in os.listdir('starting_point/Full-version/Train/'):
-    X.append(img_to_array(load_img('starting_point/Full-version/Train/'+filename)))
+for filename in os.listdir('starting_point/Beta-version/Paisajes_train/'):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        img = Image.open('starting_point/Beta-version/Paisajes_train/' + filename)
+        img = img.resize((256, 256))  # Asegurar que todas las imÃ¡genes tengan las mismas dimensiones
+        X.append(img_to_array(img))
 X = np.array(X, dtype=float)
+
+
 
 # Set up train and test data
 split = int(0.95 * len(X))
 Xtrain = X[:split]
 Xtrain = 1.0 / 255 * Xtrain
+
+
 model = Sequential()
 model.add(InputLayer(input_shape=(256, 256, 1)))
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
@@ -56,16 +62,13 @@ model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(512, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(2, (3, 3), activation='sigmoid', padding='same')) #sigmoid -> activació
+model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 model.add(UpSampling2D((2, 2)))
-model.compile(optimizer='rmsprop', loss='mse')
+model.compile(optimizer='adagrad', loss='mse', metrics=['accuracy'])
 
 
 #------------------------------------------DATA LOADER--------------------------------------------------------------
@@ -84,18 +87,15 @@ batch_size = 10
 def image_a_b_gen(batch_size):
     for batch in datagen.flow(Xtrain, batch_size=batch_size):  #entrena per bloc
         lab_batch = rgb2lab(batch)
-        X_batch = lab_batch[:,:,:,0]
-        Y_batch = lab_batch[:,:,:,1:] / 128
-        yield (X_batch.reshape(X_batch.shape+(1,)), Y_batch)
+        X_batch = lab_batch[:, :, :, 0]
+        Y_batch = lab_batch[:, :, :, 1:] / 128
+        yield (X_batch.reshape(X_batch.shape + (1,)), Y_batch)  #retorna cada bloc de dades de train
 
 
-# Collect training loss history
-history = model.fit_generator(
-    image_a_b_gen(batch_size),
-    callbacks=[TensorBoard(log_dir="starting_point/Beta-version/output/first_run")],
-    epochs=300,
-    steps_per_epoch=20
-)
+#-------------------------------------------------------------------------------------------------------------------------
+# Train model      
+tensorboard = TensorBoard(log_dir="output/first_run")
+history = model.fit_generator(image_a_b_gen(batch_size), callbacks=[tensorboard], epochs=5, steps_per_epoch=10)
 
 # Save model
 model_json = model.to_json()
@@ -114,29 +114,35 @@ plt.plot(range(1, len(losses) + 1), losses)
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
 plt.title('Training Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+plt.grid(True)
+
+plt.subplot(1, 2, 2)
+plt.plot(range(1, len(accuracies) + 1), accuracies)
+plt.xlabel('Iteration')
+plt.ylabel('Accuracy')
+plt.title('Training Accuracy')
+plt.grid(True)
 
 plt.tight_layout()
 plt.savefig('starting_point/Beta-version/result/learning_curves.png')
 plt.close()
 
 # Test images
-Xtest = rgb2lab(1.0/255*X[split:])[:,:,:,0]
-Xtest = Xtest.reshape(Xtest.shape+(1,))
-Ytest = rgb2lab(1.0/255*X[split:])[:,:,:,1:]
+Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
+Xtest = Xtest.reshape(Xtest.shape + (1,))
+Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
 Ytest = Ytest / 128
 print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
 
 color_me = []
-for filename in os.listdir('starting_point/Full-version/Test/'):
-    color_me.append(img_to_array(load_img('starting_point/Full-version/Test/'+filename)))
+for filename in os.listdir('starting_point/Beta-version/Paisajes2/'):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        img = Image.open('starting_point/Beta-version/Paisajes2/' + filename)
+        img = img.resize((256, 256))
+        color_me.append(img_to_array(img))
 color_me = np.array(color_me, dtype=float)
 color_me = rgb2lab(1.0 / 255 * color_me)[:, :, :, 0]
 color_me = color_me.reshape(color_me.shape + (1,))
-
 
 # Test model
 output = model.predict(color_me)
@@ -145,7 +151,8 @@ output = output * 128
 # Output colorizations
 for i in range(len(output)):
     cur = np.zeros((256, 256, 3))
-    cur[:,:,0] = color_me[i][:,:,0]
-    cur[:,:,1:] = output[i]
-    imsave("result/img_"+str(i)+".png", lab2rgb(cur))
-
+    cur[:, :, 0] = color_me[i][:, :, 0]
+    cur[:, :, 1:] = output[i]
+    cur = lab2rgb(cur)
+   
+    imsave("starting_point/Beta-version/result/img_" + str(i) + ".png", cur)

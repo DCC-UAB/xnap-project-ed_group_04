270b86ac01c338caf20dcfbd0b5fa5081dd1111f
1605592 <1605592@uab.cat>
2023-06-04 11:57:27 +0000

    final

--- starting_point/Beta-version/beta_version.py
+++ starting_point/Beta-version/beta_version.py
@@ -30,9 +30,9 @@ import tensorflow as tf
 from skimage import img_as_ubyte
 from PIL import Image
 from keras import optimizers
-from sklearn.model_selection import train_test_split
 
 
+# Verificar la disponibilidad de la GPU
 if tf.config.experimental.list_physical_devices('GPU'):
     print('Se encontró una GPU')
 else:
@@ -46,24 +46,15 @@ if len(physical_devices) > 0:
 
 # Get images
 X = []
-
-for filename in os.listdir('starting_point/Beta-version/Train_beta/'):
+for filename in os.listdir('starting_point/Beta-version/Paisaje_train/'):
     if filename.endswith(".jpg") or filename.endswith(".png") or  filename.endswith(".jpeg"):
-        img = Image.open('starting_point/Beta-version/Train_beta/' + filename)
-
+        img = Image.open('starting_point/Beta-version/Paisaje_train/' + filename)
         img = img.resize((256, 256))  # Asegurar que todas las imÃ¡genes tengan las mismas dimensiones
         if img.mode == 'L':
             img = np.expand_dims(img, axis=2)  # Agregar una dimensión de canal
-            img = np.repeat(img, 3, axis=2) 
+            img = np.repeat(img, 3, axis=2)  
         X.append(img_to_array(img))
-        
-shapes = [img.shape for img in X]
-unique_shapes = set(shapes)
-print(unique_shapes)
-if len(unique_shapes) > 1:
-    print("Las imágenes no tienen la misma forma después de redimensionar.")
-
-X = np.array(X, dtype=float)
+print("LLISTA-------------------", len(X))
 
 # Verificar que todas las imágenes tengan la misma forma
 shapes = [img.shape for img in X]
@@ -74,13 +65,17 @@ if len(unique_shapes) > 1:
 
 X = np.array(X, dtype=float)
 
+
+
 # Set up train and test data
 split = int(0.95 * len(X))
 Xtrain = X[:split]
 Xtrain = 1.0 / 255 * Xtrain
 
-
-
+Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
+Xtest = Xtest.reshape(Xtest.shape + (1,))
+Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
+Ytest = Ytest / 128
 
 # Crear una sesión de TensorFlow y asignar la GPU como dispositivo de ejecución
 with tf.device('/GPU:0'):
@@ -118,88 +113,71 @@ with tf.device('/GPU:0'):
         shear_range=0.2,
         zoom_range=0.2,
         rotation_range=20,
-
+    # brightness_range=[0.8, 1.2],  # Random brightness adjustment
+    # channel_shift_range=20,  # Random channel shifts
+    # preprocessing_function=gaussian_noise  # Custom function for adding Gaussian noise
     )
 
 
+            #horizontal_flip=True, #false
+        # vertical_flip=True)#false
+
     # Generate training data
-    batch_size = 20 #ho he canviat
-    """
+    batch_size = 15 #ho he canviat
     def image_a_b_gen(batch_size):
         for batch in datagen.flow(Xtrain, batch_size=batch_size):  #entrena per bloc
             lab_batch = rgb2lab(batch)
             X_batch = lab_batch[:, :, :, 0]
             Y_batch = lab_batch[:, :, :, 1:] / 128
             yield (X_batch.reshape(X_batch.shape + (1,)), Y_batch)  #retorna cada bloc de dades de train
-"""
-    def image_a_b_gen(batch_size, is_validation=False):
-        if is_validation:
-            batch = Xtest
-        else:
-            batch = Xtrain
 
-        for i in range(0, len(batch), batch_size):
-            batch_images = batch[i:i+batch_size]
-            lab_batch = rgb2lab(batch_images)
-            X_batch = lab_batch[:, :, :, 0]
-            Y_batch = lab_batch[:, :, :, 1:] / 128
-            yield (X_batch.reshape(X_batch.shape + (1,)), Y_batch)
 
     #-------------------------------------------------------------------------------------------------------------------------
-    # Train model 
-    Xtest = X[split:]
-    Xtest = 1.0 / 255 * Xtest 
+    # Train model      
     tensorboard = TensorBoard(log_dir="output/first_run")
-    val_steps = len(Xtest) // batch_size
-    validation_data=image_a_b_gen(batch_size=batch_size, is_validation=True)
-    """
-    history = model.fit_generator(
-        image_a_b_gen(batch_size),
-        callbacks=[tensorboard],
-        epochs=5,
-        steps_per_epoch=10,
-        validation_steps=val_steps
-    )
-    """
-    train_steps = len(Xtrain) // batch_size
-    history = model.fit_generator(
-    image_a_b_gen(batch_size=batch_size),
-    validation_data=image_a_b_gen(batch_size=batch_size, is_validation=True),
-    callbacks=[tensorboard],
-    epochs=5,
-    steps_per_epoch=train_steps,
-    validation_steps=val_steps)
+    history = model.fit_generator(image_a_b_gen(batch_size), callbacks=[tensorboard], epochs=350, steps_per_epoch=50, validation_data=(Xtest, Ytest))
+
+    ########################################################################
+    print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
+
     # Save model
     model_json = model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
     model.save_weights("model.h5")
-    train_loss = history.history['loss']
+
+    # Process history
+    losses = history.history['loss']
+    loss_valida = history.history['val_loss']
+    
+    # Plot learning curves
     plt.figure(figsize=(12, 6))
-    plt.plot(range(1, len(train_loss) + 1), train_loss, label='Training Loss')
-    plt.xlabel('Iteration')
+    plt.plot(range(1, len(losses) + 1), losses, label = "Training Loss")
+    plt.plot(range(1, len(loss_valida) + 1), loss_valida, label = "Validation Loss")
+    plt.xlabel('Epochs')
     plt.ylabel('Loss')
-    plt.title('Training Loss')
+    plt.title('Training and Validation Loss')
     plt.legend()
     plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-    plt.savefig('starting_point/Beta-version/result_curves/learning_curves-rural.png')
 
+
+    plt.tight_layout()
+    plt.savefig('starting_point/Beta-version/curves/curva-prado.png')
+    plt.close()
 
     # Test images
-    Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
-    Xtest = Xtest.reshape(Xtest.shape + (1,))
-    Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
-    Ytest = Ytest / 128
-    print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
-    val_loss = model.evaluate(Xtest, Ytest, batch_size=20)
-    print(f"Pérdida de validación: {val_loss}")
-    color_me = []
+    
+    #Xtest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 0]
+    #Xtest = Xtest.reshape(Xtest.shape + (1,))
+    #Ytest = rgb2lab(1.0 / 255 * X[split:])[:, :, :, 1:]
+    #Ytest = Ytest / 128
 
-    for filename in os.listdir('starting_point/Beta-version/Val_beta/'):
+    print(model.evaluate(Xtest, Ytest, batch_size=batch_size))
+
+    color_me = []
+    for filename in os.listdir('starting_point/Beta-version/Paisajes2/'):
         if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-            img = Image.open('starting_point/Beta-version/Val_beta/' + filename)
+            img = Image.open('starting_point/Beta-version/Paisajes2/' + filename)
             img = img.resize((256, 256))
             if img.mode == 'L':
                 img = np.expand_dims(img, axis=2)  # Agregar una dimensión de canal
@@ -228,14 +206,4 @@ for i in range(len(output)):
     cur[:, :, 1:] = output[i]
     cur = lab2rgb(cur)
    
-    imsave("starting_point/Beta-version/result_rural/img_" + str(i) + ".png", cur)
-
-val_loss = history.history['val_loss']
-plt.plot(range(1, len(val_loss) + 1), val_loss, label='Validation Loss')
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
-plt.title('Validation Loss')
-plt.legend()
-plt.grid(True)
-plt.savefig('starting_point/Beta-version/result_curves/validation_loss.png')
-plt.show()
+    imsave("starting_point/Beta-version/result-prados/img_" + str(i) + ".png", cur)
